24bc4824720843b3b0237a2305984a1d7d6628c7
Nina <nina.gs20@yandex.ru>
2019-11-29 18:38:43 +0400

    Updates

---
+++ precache-manifest.2c28beb30e20d98d3cc7ad8eba66cee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8099716876e555c61752367c1f6b12",
+    "url": "/nina8891/Quiz-App/index.html"
+  },
+  {
+    "revision": "193418bb32c51aa0ca6c",
+    "url": "/nina8891/Quiz-App/static/css/main.7744d2b4.chunk.css"
+  },
+  {
+    "revision": "32d98efd61469702e763",
+    "url": "/nina8891/Quiz-App/static/js/2.e90cce1e.chunk.js"
+  },
+  {
+    "revision": "193418bb32c51aa0ca6c",
+    "url": "/nina8891/Quiz-App/static/js/main.4eba4008.chunk.js"
+  },
+  {
+    "revision": "0139ec6bc2810969303b",
+    "url": "/nina8891/Quiz-App/static/js/runtime-main.7e1789c2.js"
+  }
+]);
